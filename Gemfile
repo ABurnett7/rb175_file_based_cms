8a13d0badd98e3564cf30e93649a932e15ef8bc6
Andy Burnett <38389218+ABurnett7@users.noreply.github.com>
2025-05-04 12:05:42 -0600

    Adding basic minitests

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "erubi"
+
 gem "webrick"
+gem "rack-test"
+gem "minitest"
 
 ruby "3.2.2"
---
+++ test/cms_test.rb
@@ -0,0 +1,44 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+  def test_history_txt
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_equal File.read("data/history.txt"), last_response.body
+  end
+
+  def test_changes_txt
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_equal File.read("data/changes.txt"), last_response.body
+  end
+
+  def test_about_txt
+    get "/about.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_equal File.read("data/about.txt"), last_response.body
+  end
+end
