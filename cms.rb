57ddcd4a5d67922ad2dcf42c6d44532e64d7239b
Andy Burnett <38389218+ABurnett7@users.noreply.github.com>
2025-05-04 10:34:52 -0600

    Adding an Index Page

--- cms.rb
+++ cms.rb
@@ -3,7 +3,11 @@ require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubi"
 
+root = File.expand_path("..", __FILE__)
 
 get "/" do
-  "Getting Started"
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
