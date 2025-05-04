50cb9ad5b67fc6215492b42849f2033d47aa8e8f
Andy Burnett <38389218+ABurnett7@users.noreply.github.com>
2025-05-04 14:50:43 -0600

    created separate testing environment

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -8,34 +8,57 @@ require_relative "../cms"
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
 
-  def app
+    def app
     Sinatra::Application
   end
 
-  def test_index
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
+def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.md"    
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
+
   end
 
   def test_history_txt
+    create_document "history.txt", "1993"
+    
     get "/history.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
-    assert_equal File.read("data/history.txt"), last_response.body
+    assert_includes last_response.body, "1993"
   end
 
   def test_changes_txt
+    create_document "changes.txt", "changes be here"
+      
     get "/changes.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
-    assert_equal File.read("data/changes.txt"), last_response.body
+    assert_includes last_response.body, "changes"
   end
 
   def test_viewing_markdown_document
+    create_document "about.md", "<h1>Ruby is...</h1>"
     get "/about.md"
 
     assert_equal 200, last_response.status
@@ -60,6 +83,7 @@ class CMSTest < Minitest::Test
 
   # test/cms_test.rb
   def test_editing_document
+    create_document "/changes.txt"
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
@@ -68,6 +92,8 @@ class CMSTest < Minitest::Test
   end
 
   def test_updating_document
+    create_document "/changes.txt"
+
     post "/changes.txt", content: "new content"
 
     assert_equal 302, last_response.status
